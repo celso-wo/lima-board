83d6ecb7c1045a05f23a3ec4fe8d363ae5fa018d
Celso Wo <celsowo@gmail.com>
2016-10-29 23:05:02 -0200

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
+var index = require('./controllers/index');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get('/', index.index);
+app.get('/profile', index.profile);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
---
+++ src/controllers/index.js
@@ -0,0 +1,36 @@
+var phantom = require('phantom');
+
+exports.index = function(req, res) {
+  var profiles = [
+    'https://github.com/celso-wo',
+    'https://github.com/felipebernardes',
+    'https://github.com/GabrielJacquier',
+    'https://github.com/mikemajesty',
+    'https://github.com/samfrezza'
+  ];
+
+  res.render('pages/index', { profiles: profiles});
+};
+
+exports.profile = function(req, res) {
+  phantom.create().then(function(ph) {
+    ph.createPage().then(function(page) {
+      page.open(req.query.url).then(function(status) {
+        page.evaluate(function() {
+          return {
+            avatar: document.querySelector('img.avatar').outerHTML,
+            calendar: document.querySelector('[data-graph-url]').outerHTML,
+            vcard: document.querySelector('.vcard-names').outerHTML,
+          };
+        }).then(function(data) {
+          page.close();
+          ph.exit();
+
+          data.url = req.query.url;
+
+          res.render('pages/profile', data);
+        });
+      });
+    });
+  });
+};
